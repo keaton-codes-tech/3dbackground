e14b450249e0752e48a9c8f7a361e6318dfdeea2
oldbootz <internet@tachyonwireless.co.za>
2021-12-30 09:23:10 +0200

    testing new point generation method
    Not as fast as the pure random plot

--- src/index.ts
+++ src/index.ts
@@ -40,45 +40,59 @@ const maxTimeToWait = 2000;
 const beginTime = Date.now();
 let elapsed = 0;
 
-// Create first points
-const firstPoint = new Point(canvas.width/2, canvas.height/2);
-points.push(firstPoint);
-// points.push({x: -100, y: -100});
-// points.push({x: -100, y: canvas.height + 100});
-// points.push({x: canvas.width + 100, y: -100});
-// points.push({x: canvas.width + 100, y: canvas.height + 100});
+function generatePoints() {
+    // Create first points
+    const firstPoint = new Point(canvas.width/2, canvas.height/2);
+    points.push(firstPoint);
+    
+    // points.push({x: -100, y: -100});
+    // points.push({x: -100, y: canvas.height + 100});
+    // points.push({x: canvas.width + 100, y: -100});
+    // points.push({x: canvas.width + 100, y: canvas.height + 100});
 
-// Create remaining points
-while (elapsed < maxTimeToWait && points.length < numPoints) {
-    elapsed = Date.now() - beginTime;
-    console.log(elapsed);
-    const x = intBetweenRange(-100, canvas.width + 100);
-    const y = intBetweenRange(-100, canvas.height + 100);
-    const point = new Point(x, y);
-    let isValid = false;
-    for (let i = 0; i < points.length; i++) {
-        const p = points[i];
-        const distance = Math.sqrt(Math.pow(p.x - point.x, 2) + Math.pow(p.y - point.y, 2));
-        if (distance > pointMinDistance && distance < pointMaxDistance) {
-            isValid = true;
-        } else if (distance <= pointMinDistance) {
-            isValid = false;
-            break;
+    // Create remaining points
+    while (elapsed < maxTimeToWait && points.length < numPoints) {
+        elapsed = Date.now() - beginTime;
+        console.log(elapsed);
+        // choose a random existing point
+        let sourcePoint: PointInterface;
+        if (points.length < 4) {
+            sourcePoint = points[intBetweenRange(0, points.length - 1)];
+            addPoint(sourcePoint);
+        } else {
+            // choose a random existing convex hull point
+            const coords = convertPointsToCoords(points);
+            const delaunay = Delaunator.from(coords);
+            sourcePoint = points[delaunay.hull[intBetweenRange(0, delaunay.hull.length - 1)]];
+            addPoint(sourcePoint);
         }
-    }
-    if (isValid) {
-        points.push(point);
+        function addPoint(sourcePoint: PointInterface) {
+            const validPoint = findNewPoint(sourcePoint);
+            if (validPoint) {
+                points.push(validPoint);
+            }            
+        }
+
     }
 }
 
-// Change array shape for delaunay library
-const coords = points.map((x) => {return [Math.floor(x.x), Math.floor(x.y)];});
+// Change array shape for delaunay library - [[x, y], [x, y], [x, y]]
+function convertPointsToCoords(points: Array<PointInterface>) {
+    const coords: Array<Array<number>> = [];
+    for (let i = 0; i < points.length; i++) {
+        const point = points[i];
+        coords.push([Math.floor(point.x), Math.floor(point.y)]);
+    }
+    return coords;
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // connect all points without crossing lines using delaunay triangulation
+    const coords = convertPointsToCoords(points);
     const delaunay = Delaunator.from(coords);
     console.log(delaunay);
+    console.log(points);
     const triangles = delaunay.triangles;
     ctx.beginPath();
     for (let i = 0; i < triangles.length; i += 3) {
@@ -99,6 +113,42 @@ function draw() {
     }
 }
 
+function findNewPoint(sourcePoint: PointInterface) {
+    const theta = Math.random() * 2 * Math.PI;
+    const radius = intBetweenRange(pointMinDistance, pointMaxDistance);
+    const x = sourcePoint.x + radius * Math.cos(theta);
+    const y = sourcePoint.y + radius * Math.sin(theta);
+    const newPoint = new Point(x, y);
+    if (isValidPoint(newPoint)) {
+        return newPoint;
+    } else {
+        return false;
+    }
+}
+
+function isValidPoint(point: PointInterface) {
+    if (point.x < 0 || point.x > canvas.width || point.y < 0 || point.y > canvas.height) {
+        return false;
+    } else {
+        let isValid = false;
+        for (let i = 0; i < points.length; i++) {
+            const p = points[i];
+            const distance = Math.sqrt(Math.pow(p.x - point.x, 2) + Math.pow(p.y - point.y, 2));
+            if (distance > pointMinDistance && distance < pointMaxDistance) {
+                isValid = true;
+            } else if (distance <= pointMinDistance) {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
 function intBetweenRange(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -108,4 +158,8 @@ function loop() {
     //window.requestAnimationFrame(loop);
 }
 
-loop();
+function init() {
+    generatePoints();
+    loop();
+}
+init();
