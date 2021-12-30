320022a23578bae62b97cd01f4ab096762f479da
oldbootz <internet@tachyonwireless.co.za>
2021-12-30 15:39:43 +0200

    Fill triangles method

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,24 @@ function main() {
         x: number;
         y: number;
     }
+    type CoordsType = Array<Array<number>>;
 
     // Classes
     class Point implements PointInterface {
         x: number;
         y: number;
-        constructor(x: number, y: number){
+        constructor(x: number, y: number) {
             this.x = x;
             this.y = y;
         }
     }
 
     // Canvas and context
-    const canvas: HTMLCanvasElement = document.createElement("canvas");
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
     document.body.appendChild(canvas);
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    const ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
+    const ctx = <CanvasRenderingContext2D>canvas.getContext('2d');
 
     // Draw style
     ctx.lineJoin = 'round';
@@ -50,9 +51,12 @@ function main() {
     function generatePoints() {
         console.time('Generate points function');
         // Create first points
-        const firstPoint = new Point(Math.floor(canvas.width/2), Math.floor(canvas.height/2));
+        const firstPoint = new Point(
+            Math.floor(canvas.width / 2),
+            Math.floor(canvas.height / 2)
+        );
         points.push(firstPoint);
-        
+
         // points.push({x: -100, y: -100});
         // points.push({x: -100, y: canvas.height + 100});
         // points.push({x: canvas.width + 100, y: -100});
@@ -60,7 +64,11 @@ function main() {
 
         // Create remaining points
         const pointsPopulated: Array<number> = [];
-        while (elapsed < maxTimeToWait && points.length < numPoints && pointsPopulated.length < points.length) {
+        while (
+            elapsed < maxTimeToWait &&
+            points.length < numPoints &&
+            pointsPopulated.length < points.length
+        ) {
             elapsed = Date.now() - beginTime;
             //console.log(elapsed);
 
@@ -80,15 +88,21 @@ function main() {
                     // choose a random existing convex hull point
                     const coords = convertPointsToCoords(points);
                     const delaunay = Delaunator.from(coords);
-                    sourcePoint = points[delaunay.hull[intBetweenRange(0, delaunay.hull.length - 1)]];
+                    sourcePoint =
+                        points[
+                            delaunay.hull[
+                                intBetweenRange(0, delaunay.hull.length - 1)
+                            ]
+                        ];
                     addPoint(sourcePoint);
                 }
                 function addPoint(sourcePoint: PointInterface) {
-                    const validPoint = randomAngleFromPoint_FindNewPoint(sourcePoint);
+                    const validPoint =
+                        randomAngleFromPoint_FindNewPoint(sourcePoint);
                     if (validPoint) {
                         points.push(validPoint);
                         //console.log('point added');
-                    }            
+                    }
                 }
             }
 
@@ -98,12 +112,13 @@ function main() {
                 sourcePoint = points[intBetweenRange(0, points.length - 1)];
                 addPoint(sourcePoint);
                 function addPoint(sourcePoint: PointInterface) {
-                    const validPoint = randomAngleFromPoint_FindNewPoint(sourcePoint);
+                    const validPoint =
+                        randomAngleFromPoint_FindNewPoint(sourcePoint);
                     if (validPoint) {
                         points.push(validPoint);
                         //console.log('point added');
-                    }            
-                }            
+                    }
+                }
             }
 
             function multipleRandomAnglesFromPoint() {
@@ -117,8 +132,11 @@ function main() {
                     if (count > 10) {
                         return;
                     } else if (pointsPopulated.includes(sourcePointIndex)) {
-                        sourcePointIndex = intBetweenRange(0, points.length - 1);
-                        evaluateSourcePointIndex(count);                    
+                        sourcePointIndex = intBetweenRange(
+                            0,
+                            points.length - 1
+                        );
+                        evaluateSourcePointIndex(count);
                     } else {
                         acceptableSourcePointIndex = true;
                     }
@@ -132,12 +150,13 @@ function main() {
                         addPoint(sourcePoint);
                     }
                     function addPoint(sourcePoint: PointInterface) {
-                        const validPoint = randomAngleFromPoint_FindNewPoint(sourcePoint);
+                        const validPoint =
+                            randomAngleFromPoint_FindNewPoint(sourcePoint);
                         if (validPoint) {
                             points.push(validPoint);
                             //console.log('point added');
-                        }            
-                    }                
+                        }
+                    }
                 }
             }
             multipleRandomAnglesFromPoint();
@@ -148,7 +167,7 @@ function main() {
 
     // Change array shape for delaunay library - [[x, y], [x, y], [x, y]]
     function convertPointsToCoords(points: Array<PointInterface>) {
-        const coords: Array<Array<number>> = [];
+        const coords: CoordsType = [];
         for (let i = 0; i < points.length; i++) {
             const point = points[i];
             coords.push([point.x, point.y]);
@@ -163,8 +182,7 @@ function main() {
         const coords = convertPointsToCoords(points);
         const delaunay = Delaunator.from(coords);
 
-        // console.log(delaunay);
-        // console.log(points);
+        console.log(delaunay);
 
         // draw lines to make up the triangles
         function drawTriangles() {
@@ -178,11 +196,24 @@ function main() {
                 ctx.moveTo(coords[p0][0], coords[p0][1]);
                 ctx.lineTo(coords[p1][0], coords[p1][1]);
                 ctx.lineTo(coords[p2][0], coords[p2][1]);
-                ctx.closePath();            
+                ctx.closePath();
             }
-            ctx.stroke();        
+            ctx.stroke();
         }
         drawTriangles();
+
+        function fillTriangles() {
+            forEachTriangle(coords, delaunay, (t: number, threePoints: CoordsType) => {
+                ctx.fillStyle = 'rgb(50,50,50)';
+                ctx.beginPath();
+                ctx.moveTo(threePoints[0][0], threePoints[0][1]);
+                ctx.lineTo(threePoints[1][0], threePoints[1][1]);
+                ctx.lineTo(threePoints[2][0], threePoints[2][1]);
+                ctx.closePath();
+                ctx.fill();
+            });
+        }
+        fillTriangles();
 
         // draw lines to make up the convex hull
         function drawHull() {
@@ -197,7 +228,7 @@ function main() {
                     ctx.lineTo(coords[hull[0]][0], coords[hull[0]][1]);
                 }
             }
-            ctx.stroke();        
+            ctx.stroke();
         }
 
         // draw points after the lines so that they dont get painted over
@@ -206,7 +237,7 @@ function main() {
             for (let i = points.length - 1; i >= 0; i--) {
                 const point = points[i];
                 ctx.fillRect(point.x, point.y, 1, 1);
-            }        
+            }
         }
         drawPoints();
         console.timeEnd('Draw function');
@@ -237,14 +268,24 @@ function main() {
     }
 
     function isValidPoint(point: PointInterface) {
-        if (point.x < xStart || point.x > xEnd || point.y < yStart || point.y > yEnd) {
+        if (
+            point.x < xStart ||
+            point.x > xEnd ||
+            point.y < yStart ||
+            point.y > yEnd
+        ) {
             return false;
         } else {
             let isValid = false;
             for (let i = 0; i < points.length; i++) {
                 const p = points[i];
-                const distance = Math.sqrt(Math.pow(p.x - point.x, 2) + Math.pow(p.y - point.y, 2));
-                if (distance > pointMinDistance && distance < pointMaxDistance) {
+                const distance = Math.sqrt(
+                    Math.pow(p.x - point.x, 2) + Math.pow(p.y - point.y, 2)
+                );
+                if (
+                    distance > pointMinDistance &&
+                    distance < pointMaxDistance
+                ) {
                     isValid = true;
                 } else if (distance <= pointMinDistance) {
                     isValid = false;
@@ -263,6 +304,29 @@ function main() {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
+    function edgesOfTriangle(t: number) {
+        return [3 * t, 3 * t + 1, 3 * t + 2];
+    }
+
+    function pointsOfTriangle(delaunay: Delaunator<ArrayLike<number>>, t: number) {
+        return edgesOfTriangle(t).map((e) => {
+            return delaunay.triangles[e];
+        });
+    }
+
+    function forEachTriangle(coords: CoordsType, delaunay: Delaunator<ArrayLike<number>>, callback: Function) {
+        // basically just index shuffling through different arrays
+        // t = triangle index
+        for (let t = 0; t < delaunay.triangles.length / 3; t++) {
+            callback(
+                t,
+                pointsOfTriangle(delaunay, t).map((p) => {
+                    return coords[p];
+                })
+            );
+        }
+    }
+
     function updateFPS() {
         const fpsElement = document.getElementById('fps-counter');
         if (fpsElement) {
@@ -275,7 +339,7 @@ function main() {
     function loop(t0 = performance.now()) {
         draw();
         fps = Math.floor(1000 / (performance.now() - t0));
-        requestAnimationFrame(loop);
+        //requestAnimationFrame(loop);
     }
 
     function init() {
